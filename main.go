c86733d805098ec94714456d331205db413c3a4f
pezy4303675 <maracujapezy@gmail.com>
2025-07-14 10:23:14 +0100

    dasd

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	bytes, _ := os.ReadFile("./exemples/00.lang")
+	source := string(bytes)
+
+	fmt.Printf("Code: %s\n", source)
 }
